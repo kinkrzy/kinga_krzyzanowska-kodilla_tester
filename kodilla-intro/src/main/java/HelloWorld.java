af0bfc5bab16567ea118ddd3d3afa9ad38c226a6
Kinga Krzyzanowska <kinga.krzyzanowskaa@gmail.com>
2022-04-06 13:54:29 +0200

    task Hello World

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+    public static void main(String[] arg) {
+        String name = "Kinga";
+        int age = 29;
+        char surname1stLetter = 'K';
+        System.out.println("Hello World"+System.lineSeparator()+name+System.lineSeparator()+age+System.lineSeparator()+surname1stLetter+System.lineSeparator());
+
+
+    }
+
+}
