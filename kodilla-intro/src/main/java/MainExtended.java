97f044fb42e5a04e6d489080db19fa38897c3bfc
Kinga Krzyzanowska <kinga.krzyzanowskaa@gmail.com>
2022-04-06 16:40:57 +0200

    task 2 Variables

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,12 @@
+public class MainExtended {
+    public static void main(String[] arg) {
+        String species= "Hydrochoerus hydrochaeris" ;
+                double maxLength= 1.34 ;
+                        int maxWeight= 91 ;
+                                int maxLifespan = 12 ;
+                                boolean isMammal= true ;
+                                        double maxHeight = 0.62 ;
+    System.out.println(species+System.lineSeparator() + maxHeight+System.lineSeparator() + maxLength+System.lineSeparator() +maxWeight+System.lineSeparator()+ maxLifespan+System.lineSeparator() + isMammal+System.lineSeparator() );
+
+    }
+}
