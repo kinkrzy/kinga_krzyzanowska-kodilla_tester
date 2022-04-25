9b1fb602753096a77d9a03fa14a888dacfe59d67
Kinga Krzyzanowska <kinga.krzyzanowskaa@gmail.com>
2022-04-25 02:33:19 +0200

    task 2 Variables

---
+++ kodilla-intro/src/main/java/Application.java
@@ -0,0 +1,11 @@
+public class Application {
+    public static void main(String[] args) {
+        UserData userData = new UserData("Adam",40.5,178);
+        {
+            if (userData.age > 30 && userData.height > 160)
+                System.out.println("User is older than 30 and taller than 160cm");
+            if (userData.age <30 && userData.height <160)
+                System.out.println("User is 30 (or younger) or 160cm (or shorter)");
+            }
+        }
+    }
---
+++ kodilla-intro/src/main/java/UserData.java
@@ -0,0 +1,13 @@
+public class UserData {
+
+        String name;
+        double age;
+        double height;
+
+        public UserData(String name, double age, double height) {
+            this.name = name;
+            this.age = age;
+            this.height = height;
+        }
+    }
+
